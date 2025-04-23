3e9f1f603d74275708773f6299a824b63447cb4c
Akash Das <akashdas.dev@gmail.com>
2025-04-23 23:35:54 +0600

    initial page added

---
+++ lib/presentation/screen/main_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class MainView extends StatelessWidget {
+  const MainView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
